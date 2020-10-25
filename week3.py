4257e30ffe60bea354bf344e868f715e5195c59c
태형 이 <plus100kt@gmail.com>
2020-10-26 02:06:09 +0900

    feat(week3): insert data in music db

--- week3.py
+++ week3.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.sparta
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
@@ -19,3 +23,10 @@ for tr in trs:
     title = tr.select_one('.title').text.strip()
     artist = tr.select_one('.artist').text.strip()
     print(rank, title, artist)
+
+    doc = {
+        'rank': rank,
+        'title': title,
+        'artist': artist
+    }
+    db.musics.insert_one(doc)
